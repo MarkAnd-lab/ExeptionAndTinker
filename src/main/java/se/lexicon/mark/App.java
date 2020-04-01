07413752e9bac1f33e7f59b57c69e33088fdbc1f
MarkAnd-lab <mark-andersson@hotmail.com>
2020-04-01 08:55:05 +0200

    added input

--- src/main/java/se/lexicon/mark/App.java
+++ src/main/java/se/lexicon/mark/App.java
@@ -1,10 +1,21 @@
 package se.lexicon.mark;
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main(String[] args) {
-        double x = 100;
-        double y = 0;
+        //double x = 100;
+        //double y = 0;
+
+        Scanner scan = new Scanner(System.in);
+        double x;
+        double y;
+
+        System.out.println("Enter first number: ");
+        x = scan.nextDouble();
+        System.out.println("Enter second number to divide with: ");
+        y = scan.nextDouble();
 
         try {
             if (y == 0){
